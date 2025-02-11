c28380cb3d3b31c85e8f784a303b3ea170056df8
Ashref0 <achref.jebali@esprit.tn>
2025-02-11 14:47:33 +0100

    second push

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,11 @@ import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { LoginComponent } from './pages/login/login.component';
 import { InscriptionComponent } from './pages/inscription/inscription.component';
 import { LayoutComponent } from './pages/layout/layout.component';
+import { DashboardProfileComponent } from './pages/dashboard-profile/dashboard-profile.component';
+import { DashboardSettingsComponent } from './pages/dashboard-settings/dashboard-settings.component';
+import { FooterComponent } from './pages/footer/footer.component';
+
+
 
 
 
@@ -14,6 +19,13 @@ export const routes: Routes = [  // Make sure to export 'routes'
   { path: 'signup', component: InscriptionComponent },
   { path: 'dashboard', component: DashboardComponent },
   { path: 'layout', component: LayoutComponent },
+  { path: 'dashboard-profile', component: DashboardProfileComponent },
+  { path: 'dashboard-profile', component: DashboardProfileComponent },
+  { path: 'dashboard-settings', component: DashboardSettingsComponent },
+  { path: 'footer', component: FooterComponent },
+
+
+
 
 
 ];
--- src/app/pages/inscription/inscription.component.ts
+++ src/app/pages/inscription/inscription.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { FooterComponent } from '../footer/footer.component';
 import { LayoutComponent } from '../layout/layout.component';
 
 
 @Component({
   selector: 'app-inscription',
   standalone: true,
-  imports: [LayoutComponent],
+  imports: [LayoutComponent,FooterComponent],
   templateUrl: './inscription.component.html',
   styleUrl: './inscription.component.css'
 })
